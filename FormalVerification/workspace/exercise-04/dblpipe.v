68f39f5d6f227f8d1a5e3de8f974386c20a57c3c
fjpolo <fjpolo@gmail.com>
2024-04-18 15:17:23 +0200

    [FormalVerification][exercise-04] Need to finish verification for flsr_fib.v

--- FormalVerification/workspace/exercise-04/dblpipe.v
+++ FormalVerification/workspace/exercise-04/dblpipe.v
@@ -1,57 +1,53 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Filename: 	dblpipe.v
-// {{{
-// Project:	A set of Yosys Formal Verification exercises
-//
-// Background:	This file contains a pair of LFSR modules.  They "should" be
-//		identical.
-//
-// To prove:
-//
-//	1. That nothing changes as long as CE is low
-//
-//	2. That the outputs of the two LFSR's are identical, and hence the
-//		output, o_data, will be forever zero.
-//
-//
-// Creator:	Dan Gisselquist, Ph.D.
-//		Gisselquist Technology, LLC
-//
-////////////////////////////////////////////////////////////////////////////////
-// }}}
-// Copyright (C) 2017-2021, Gisselquist Technology, LLC
-// {{{
-// This program is free software (firmware): you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or (at
-// your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
-// target there if the PDF file isn't present.)  If not, see
-// <http://www.gnu.org/licenses/> for a copy.
-// }}}
-// License:	GPL, v3, as defined and found on www.gnu.org,
-// {{{
-//		http://www.gnu.org/licenses/gpl.html
-//
-////////////////////////////////////////////////////////////////////////////////
-//
+/*******************************************************************************
+/*
+/* Filename: 	dblpipe.v
+/* 
+/* Project:	A set of Yosys Formal Verification exercises
+/*
+/* Background:	This file contains a pair of LFSR modules.  They "should" be
+/*		identical.
+/*
+/* To prove:
+/*
+/*	1. That nothing changes as long as CE is low
+/*
+/*	2. That the outputs of the two LFSR's are identical, and hence the
+/*		output, o_data, will be forever zero.
+/*
+/*
+/* Creator:	Dan Gisselquist, Ph.D.
+/*		Gisselquist Technology, LLC
+/*
+/*******************************************************************************
+/* 
+/* Copyright (C) 2017-2021, Gisselquist Technology, LLC
+/*
+/* This program is free software (firmware): you can redistribute it and/or
+/* modify it under the terms of the GNU General Public License as published
+/* by the Free Software Foundation, either version 3 of the License, or (at
+/* your option) any later version.
+/*
+/* This program is distributed in the hope that it will be useful, but WITHOUT
+/* ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+/* for more details.
+/*
+/* You should have received a copy of the GNU General Public License along
+/* with this program.  (It's in the $(ROOT)/doc directory.  Run make with no
+/* target there if the PDF file isn't present.)  If not, see
+/* <http://www.gnu.org/licenses/> for a copy.
+/* 
+/* License:	GPL, v3, as defined and found on www.gnu.org,
+/* 
+/*		http://www.gnu.org/licenses/gpl.html
+/*
+/*******************************************************************************/
 `default_nettype	none
-// }}}
+
 module dblpipe (
-		// {{{
 		input	wire	i_clk, i_ce,
-		//
 		input	wire	i_data,
 		output	reg	o_data
-		// }}}
 	);
 
 	wire	a_data, b_data;
@@ -62,4 +58,30 @@ module dblpipe (
 	initial	o_data = 1'b0;
 	always @(posedge i_clk)
 		o_data <= a_data ^ b_data;
+
+`ifdef FORMAL
+
+	// f_past_valid
+	reg	f_past_valid;
+	initial	f_past_valid = 1'b0;
+	always @(posedge i_clk)
+		f_past_valid <= 1'b1;
+
+	//  1. Prove that nothing changes as long as CE is low
+	always @(posedge i_clk) begin
+		if((f_past_valid)&&(!i_ce)&&($past(!i_ce))) begin
+			assert(a_data == $past(a_data));
+			assert(b_data == $past(b_data));
+		end
+	end
+	//  2. Prove that the outputs of the two LFSR's are identical, 
+	// and hence the output, o_data, will be forever zero.
+	always @(posedge i_clk)
+		if((f_past_valid)&&(!i_ce)&&($past(!i_ce))&&(a_data == b_data)) begin
+			assert(!o_data);
+		end
+
+`endif
+
+
 endmodule
